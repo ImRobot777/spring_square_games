e44e122d95dfc4037c7c109660b221667396e704
Guillaume GUITARIAN <guillaume.guitarian@le-campus-numerique.fr>
2026-09-10 12:12:18 +0200

    chore: initial commit (Spring Boot setup & gitignore)

---
+++ src/test/java/fr/campus/grog/SG/SgApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.campus.grog.SG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
